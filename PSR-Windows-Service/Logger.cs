ecd4144c84948473b707c1b959e4cf1969b11558
tamagoru <108638495+ztamagoru@users.noreply.github.com>
2023-09-08 11:50:04 -0300

    Add project files.

---
+++ PSR-Windows-Service/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.ServiceProcess;
+using System.Timers;
+
+class ServiceLogger
+{
+    public static void Log(string system, string title, string text = "")
+    {
+        string path = System.Reflection.Assembly.GetExecutingAssembly()
+               .Location + @"\..\..\..\..\PSR-Windows-Service\Resources\Log";
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        DateTime now = DateTime.Now;
+
+        string logDescription = $"{now:yyyy-MM-dd HH:mm:ss} - {system} - {title} - {text}";
+        string logName = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".log";
+        string logPath = Path.Combine(path, logName);
+
+        try
+        {
+            File.AppendAllText(logPath, logDescription + Environment.NewLine);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while saving log: {ex.Message}");
+        }
+    }
+}
---
+++ PSR-Windows-Service/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IncrementService service = new IncrementService();
+
+            try
+            {
+                ServiceBase.Run(service);
+                ServiceLogger.Log("Log", "Servicio iniciado");
+            }
+            catch (Exception ex)
+            {
+                ServiceLogger.Log("Log", "Service_Error al crear servicio", ex.Message);
+            }
+            
+        }
+    }
+}
